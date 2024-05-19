2c532cfff81712d63482ce16183425fa6abf2d9a
Zaid Aidaros <106095323+ZaidAidaros@users.noreply.github.com>
2024-05-19 15:21:31 +0300

    Update index.js

--- models/index.js
+++ models/index.js
@@ -102,7 +102,7 @@ db.Article.hasMany(db.UserBookMark);
 
 //////////// DB Sync  ///////////////////////////////////////////////////////
 
-sequelize.sync({force:true, logging:false});
+sequelize.sync({logging:false});
 initAdminUser(db);
 
 module.exports = db;
