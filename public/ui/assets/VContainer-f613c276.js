9e1c18b7b756a3172810e59446e76d244e1975f5
ZaidAidaros <zaidaidaros.lear@gmail.com>
2024-05-30 12:08:28 +0300

    add folder pub

--- public/ui/assets/VContainer-f613c276.js
+++
@@ -1,1 +0,0 @@
-import{D as n,a2 as o,a7 as r,K as l,am as i,Y as c,a as m}from"./index-53016639.js";const u=n({fluid:{type:Boolean,default:!1},...o(),...r()},"VContainer"),C=l()({name:"VContainer",props:u(),setup(a,e){let{slots:s}=e;const{rtlClasses:t}=i();return c(()=>m(a.tag,{class:["v-container",{"v-container--fluid":a.fluid},t.value,a.class],style:a.style},s)),{}}});export{C as V};
