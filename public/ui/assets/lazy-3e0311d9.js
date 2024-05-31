4a9cbc55ac6f66eecf226d2366042182c2a1d548
ZaidAidaros <zaidaidaros.lear@gmail.com>
2024-05-31 12:11:41 +0300

    add theme init

---
+++ public/ui/assets/lazy-3e0311d9.js
@@ -0,0 +1,1 @@
+import{D as n,aP as r,P as u,U as l}from"./index-f94e1acf.js";const c=n({eager:Boolean},"lazy");function m(e,o){const a=r(!1),s=u(()=>a.value||e.eager||o.value);l(o,()=>a.value=!0);function t(){e.eager||(a.value=!1)}return{isBooted:a,hasContent:s,onAfterLeave:t}}export{c as m,m as u};
