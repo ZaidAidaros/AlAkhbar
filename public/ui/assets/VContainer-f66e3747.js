249484960c7190f458959692eb00b1e3496454cb
ZaidAidaros <zaidaidaros.lear@gmail.com>
2024-05-31 11:40:31 +0300

    add theme init

--- public/ui/assets/VContainer-f66e3747.js
+++
@@ -1,1 +0,0 @@
-import{D as n,a1 as o,a6 as r,J as l,al as i,X as c,a as u}from"./index-ab7e0d88.js";const m=n({fluid:{type:Boolean,default:!1},...o(),...r()},"VContainer"),C=l()({name:"VContainer",props:m(),setup(a,e){let{slots:s}=e;const{rtlClasses:t}=i();return c(()=>u(a.tag,{class:["v-container",{"v-container--fluid":a.fluid},t.value,a.class],style:a.style},s)),{}}});export{C as V};
